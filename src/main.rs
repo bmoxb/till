d54a04d3e493e64333b8e6df6dfc3651a0091a7c
WiredSound <maxoblack@yahoo.com>
2020-05-20 16:42:51 +0100

    Convert to absolute path

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod stream;
 mod lexing;
 
-use std::{ env, fs, path::Path };
+use std::{ env, fs, io, path::Path };
 
 fn main() {
     // Only enable logging if debug build:
@@ -14,21 +14,35 @@ fn main() {
     }
 }
 
-fn execute_file(path: &Path) {
-    if let Ok(file) = fs::File::open(path) {
-        log::info!("Successfully opened file: {}", path.canonicalize().unwrap().display());
+fn execute_file(relative_path: &Path) {
+    let path = match relative_path.canonicalize() {
+        Ok(full_path) => full_path,
+        Err(_) => relative_path.to_path_buf()
+    };
 
-        let mut lxr = lexing::new_lexer();
-        lxr.stream = Some(stream::Stream::from_file(file));
+    match fs::File::open(&path) {
+        Ok(file) => {
+            log::info!("Successfully opened file: {}", path.display());
 
-        let tokens: Vec<lexing::Token> = lxr.filter_map(|result| {
-            match result {
-                lexing::lexer::LexResult::Success(_, _, tok) => Some(tok),
-                lexing::lexer::LexResult::Failure(_, _) => None
+            let mut lxr = lexing::new_lexer();
+            lxr.stream = Some(stream::Stream::from_file(file));
+
+            let tokens: Vec<lexing::Token> = lxr.filter_map(|result| {
+                match result {
+                    lexing::lexer::LexResult::Success(_, _, tok) => Some(tok),
+                    lexing::lexer::LexResult::Failure(_, _) => None
+                }
+            }).collect();
+
+            log::info!("Tokens: {:?}", tokens);
+        }
+        Err(e) => {
+            match e.kind() {
+                io::ErrorKind::NotFound => println!("File not found at: {}", path.display()),
+                io::ErrorKind::PermissionDenied => println!("Lack required permissions to read file at: {}", path.display()),
+                kind => println!("Error of kind {:?} occured when attempting to open file at: {}", kind, path.display())
             }
-        }).collect();
-
-        log::info!("Tokens: {:?}", tokens);
+        }
     }
 }
 
