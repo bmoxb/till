1cd8d39cacdf92dd6838f515fe5d927c4e8c4996
WiredSound <maxoblack@yahoo.com>
2020-06-03 16:40:50 +0100

    Create AST representation for statements, minor changes to grammar

--- src/parsing/mod.rs
+++ src/parsing/mod.rs
@@ -1,7 +1,36 @@
 pub mod parser;
 
 pub enum Statement {
-    // ...
+    If {
+        condition: Expression,
+        if_block: Vec<Statement>,
+        else_block: Option<Vec<Statement>>
+    },
+
+    FunctionDefinition {
+        identifier: String,
+        parameters: Vec<Parameter>,
+        return_type: Option<Type>,
+        body: Vec<Statement>
+    },
+
+    VariableDeclaration {
+        identifier: String,
+        var_type: Type,
+        value: Option<Expression>
+    },
+
+    VariableAssignment {
+        identifier: String,
+        value: Expression
+    }
+}
+
+pub struct Parameter (String, Type);
+
+pub enum Type {
+    Identifier(String),
+    Array(Box<Type>)
 }
 
 pub enum Expression {
